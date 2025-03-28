a66dde7955b4a6da2bff0387cddfa913018984a3
Extreme Mission <37947057+extrememission@users.noreply.github.com>
2025-03-28 07:34:13 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,25 @@
+
+const CACHE_NAME = 'bible-v1';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('bible').then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
+        '/verse.html',
         '/styles.css',
+        '/modal.css',
         '/app.js',
+        '/modal.js',
+        '/js/bookNames.js',
         '/site.webmanifest',
         '/data/kjv.json',
+        '/modal.png',
+        '/web-app-manifest-192x192.png',
+        '/web-app-manifest-512x512.png',
+        'https://fonts.googleapis.com/css2?family=Titillium+Web:ital@0;1&family=Anton&display=swap',
+        'https://fonts.googleapis.com/icon?family=Material+Icons'
       ]);
     })
   );
@@ -15,8 +27,12 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/');
+      })
   );
 });
